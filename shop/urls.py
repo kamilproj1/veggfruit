99126ac5c7d16c8b1c205a5ed08faabcc650dc78
eatvegg <projpython1@gmail.com>
2022-03-22 01:12:51 +0530

    d

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('<slug:c_slug>/',views.home,name='prodt_cat'),
     path('<slug:c_slug>/<slug:product_slug>/',views.prodDetails,name='details'),
     path('search',views.searching,name='search'),
+    path('test',views.testing,name='test'),
   ]
--- shop/views.py
+++ shop/views.py
@@ -41,3 +41,8 @@ def searching(request):
         prod=products.objects.all().filter(Q(name__contains=query)|Q(desc__contains=query))
 
     return render(request,'search.html',{'qr':query,'pr':prod})        
+
+    
+
+def testing(request):
+    return render(request,'jh.html')  
